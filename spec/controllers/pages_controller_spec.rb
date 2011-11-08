02d5ce73d66cb0f738165f00e24e8a3bb591f04e
mailshanx@yahoo.co.in <mailshanx@yahoo.co.in>
2011-11-08 15:44:25 +0800

    static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe PagesController do
-
+    render_views
+    
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+    	get 'home'
+     end
   end
 
   describe "GET 'contact'" do
@@ -15,5 +19,12 @@ describe PagesController do
       response.should be_success
     end
   end
+
+  describe "GET 'about'" do
+  	it "should be successful" do
+		get 'contact'
+		response.should be_success
+	end
+   end
 
 end
